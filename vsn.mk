31a38e3a8c4b41a9642912ea4abd81c5277bc1e0
Mikael Pettersson <mikpelinux@gmail.com>
2017-07-08 14:36:12 +0200

    v0.3

--- vsn.mk
+++ vsn.mk
@@ -1,4 +1,4 @@
-#   Copyright 2014 Mikael Pettersson
+#   Copyright 2014-2017 Mikael Pettersson
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 #
 # VSN definition for ErlScheme
 
-VSN = 0.3-devel
+VSN = 0.3
