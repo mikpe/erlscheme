9970b17a3bc29651297214841e63796fe3f71634
Mikael Pettersson <mikpelinux@gmail.com>
2022-07-14 16:02:45 +0200

    es_lexinput: rewrite as a gen_server, formatting

--- src/es_lexinput.erl
+++ src/es_lexinput.erl
@@ -1,6 +1,6 @@
 %%% -*- erlang-indent-level: 2 -*-
 %%%
-%%%   Copyright 2014 Mikael Pettersson
+%%%   Copyright 2014-2022 Mikael Pettersson
 %%%
 %%%   Licensed under the Apache License, Version 2.0 (the "License");
 %%%   you may not use this file except in compliance with the License.
@@ -21,122 +21,162 @@
 %%% the state is maintained in a separate Erlang process.
 
 -module(es_lexinput).
+-behaviour(gen_server).
 
--export([open/2,
-	 close/1,
-	 read_char/1,
-	 peek_char/1,
-	 line/1,
-	 column/1,
-	 name/1]).
+%% API
+-export([ close/1
+        , column/1
+        , line/1
+        , name/1
+        , open/2
+        , peek_char/1
+        , read_char/1
+        ]).
+
+%% gen_server callbacks
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
+        ]).
+
+-type lexinput() :: pid().
+
+%% commands
+-define(close, close).
+-define(column, column).
+-define(line, line).
+-define(name, name).
+-define(peek_char, peek_char).
+-define(read_char, read_char).
+
+%% API -------------------------------------------------------------------------
+
+-spec close(lexinput()) -> ok.
+close(LI) ->
+  call(LI, ?close).
+
+-spec column(lexinput()) -> integer().
+column(LI) ->
+  call(LI, ?column).
+
+-spec line(lexinput()) -> integer().
+line(LI) ->
+  call(LI, ?line).
+
+-spec name(lexinput()) -> string().
+name(LI) ->
+  call(LI, ?name).
+
+-spec open(es_raw_port:es_port(), string()) -> lexinput().
+open(Port, Name) ->
+  %% deliberately throw in case of error
+  {ok, Pid} = gen_server:start(?MODULE, {Port, Name}, []),
+  Pid.
+
+-spec peek_char(lexinput()) -> integer().
+peek_char(LI) ->
+  call(LI, ?peek_char).
+
+-spec read_char(lexinput()) -> integer().
+read_char(LI) ->
+  call(LI, ?read_char).
+
+%% API Internals ---------------------------------------------------------------
+
+call(Pid, Cmd) ->
+  %% deliberately throw in case of error
+  {ok, Res} = gen_server:call(Pid, Cmd, 'infinity'),
+  Res.
+
+%% gen_server callbacks --------------------------------------------------------
 
 -record(state,
-	{port,
-	 line,
-	 column,
-	 name}).
+        { port
+        , line
+        , column
+        , name
+        }).
 
-handle_loop(State) ->
-  receive
-    {Pid, Cmd} ->
-      case Cmd of
-	'read_char' ->
-	  handle_read_char(State, Pid);
-	'peek_char' ->
-	  handle_peek_char(State, Pid);
-	'line' ->
-	  handle_line(State, Pid);
-	'column' ->
-	  handle_column(State, Pid);
-	'name' ->
-	  handle_name(State, Pid);
-	'close' ->
-	  handle_close(State, Pid);
-	_ ->
-	  handle_yield_continue(State, Pid, {error, Cmd})
-      end;
+init({Port, Name}) ->
+  {ok, #state{port = Port, line = 1, column = 0, name = Name}}.
+
+handle_call(Req, _From, State) ->
+  case Req of
+    ?close ->
+      Result = handle_close(State),
+      {stop, normal, Result, []};
+    ?column ->
+      Result = handle_column(State),
+      {reply, Result, State};
+    ?line ->
+      Result = handle_line(State),
+      {reply, Result, State};
+    ?name ->
+      Result = handle_name(State),
+      {reply, Result, State};
+    ?peek_char ->
+      Result = handle_peek_char(State),
+      {reply, Result, State};
+    ?read_char ->
+      {Result, NewState} = handle_read_char(State),
+      {reply, Result, NewState};
     _ ->
-      handle_loop(State)
+      {reply, {error, {bad_call, Req}}, State}
   end.
 
-handle_yield_continue(State, Pid, R) ->
-  Pid ! {self(), R},
-  handle_loop(State).
+handle_cast(_Req, State) ->
+  {noreply, State}.
 
-%% XXX: for \r bump Line and reset Column like for \n, but also set a flag,
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State = []) -> % terminating due to explicit close
+  ok;
+terminate(_Reason, State) ->
+  _ = handle_close(State),
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% gen_server internals --------------------------------------------------------
+
+handle_close(State) ->
+  {ok, es_raw_port:close_input_port(State#state.port)}.
+
+handle_column(State) ->
+  {ok, State#state.column}.
+
+handle_line(State) ->
+  {ok, State#state.line}.
+
+handle_name(State) ->
+  {ok, State#state.name}.
+
+handle_peek_char(State) ->
+  {ok, es_raw_port:peek_char(State#state.port)}.
+
+%% FIXME: for \r bump Line and reset Column like for \n, but also set a flag,
 %% then in \n check that flag and if set do not bump Line or reset Column,
 %% for any Ch not \r reset the \r flag
 
-handle_read_char(State0, Pid) ->
-  Ch = es_raw_port:read_char(State0#state.port),
-  State1 =
+handle_read_char(State) ->
+  Ch = es_raw_port:read_char(State#state.port),
+  NewState =
     case Ch of
       $\n ->
-	Line = State0#state.line,
-	State0#state{line = Line + 1, column = 0};
+        Line = State#state.line,
+        State#state{line = Line + 1, column = 0};
       $\t ->
-	Column = State0#state.column,
-	State0#state{column = ((Column + 8) div 8) * 8};
+        Column = State#state.column,
+        State#state{column = ((Column + 8) div 8) * 8};
       -1 ->
-	State0;
+        State;
       _ ->
-	Column = State0#state.column,
-	State0#state{column = Column + 1}
+        Column = State#state.column,
+        State#state{column = Column + 1}
     end,
-  handle_yield_continue(State1, Pid, {ok, Ch}).
-
-handle_peek_char(State, Pid) ->
-  Ch = es_raw_port:peek_char(State#state.port),
-  handle_yield_continue(State, Pid, {ok, Ch}).
-
-handle_line(State, Pid) ->
-  Line = State#state.line,
-  handle_yield_continue(State, Pid, {ok, Line}).
-
-handle_column(State, Pid) ->
-  Column = State#state.column,
-  handle_yield_continue(State, Pid, {ok, Column}).
-
-handle_name(State, Pid) ->
-  Name = State#state.name,
-  handle_yield_continue(State, Pid, {ok, Name}).
-
-handle_close(State, Pid) ->
-  es_raw_port:close_input_port(State#state.port),
-  Pid ! {self(), {ok, true}}. % no continue -> terminate process
-
-command(Pid, Cmd) ->
-  MonRef = erlang:monitor('process', Pid),
-  Pid ! {self(), Cmd},
-  Res =
-    receive
-      {Pid, X} ->
-	erlang:demonitor(MonRef, ['flush']),
-	X;
-      {'DOWN', MonRef, 'process', Pid, _} ->
-	{error, noproc}
-    end,
-  {ok, Val} = Res, % deliberately throws in case of error
-  Val.
-
-open(Port, Name) ->
-  State = #state{port = Port, line = 1, column = 0, name = Name},
-  spawn(fun () -> handle_loop(State) end).
-
-read_char(Pid) ->
-  command(Pid, 'read_char').
-
-peek_char(Pid) ->
-  command(Pid, 'peek_char').
-
-line(Pid) ->
-  command(Pid, 'line').
-
-column(Pid) ->
-  command(Pid, 'column').
-
-name(Pid) ->
-  command(Pid, 'name').
-
-close(Pid) ->
-  command(Pid, 'close').
+  {{ok, Ch}, NewState}.
