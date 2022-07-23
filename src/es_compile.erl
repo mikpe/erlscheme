36572cb5bb8e6555503541517cbd6a4346fd44f0
Mikael Pettersson <mikpelinux@gmail.com>
2022-07-23 14:12:27 +0200

    es_compile: translate ES:GLOVAR to the intended F/N fname

--- src/es_compile.erl
+++ src/es_compile.erl
@@ -46,54 +46,67 @@ translate_module({'ES:MODULE', ModuleName, Exports, Defuns}) ->
   CerlModuleName = cerl:c_atom(ModuleName),
   %% Exports: add module_info/0 and module_info/1
   CerlExports = [modinfo0_fname(), modinfo1_fname() | lists:map(fun translate_export/1, Exports)],
+  %% Compute a mapping FName -> Arity from the top-level defuns
+  FEnv = build_fenv(Defuns),
   %% Translate each top-level (define (f ...) ...)
   %% Also define module_info/0 and module_info/1.
-  CerlDefuns = [modinfo0_def(ModuleName), modinfo1_def(ModuleName) | lists:map(fun translate_defun/1, Defuns)],
+  CerlDefuns = [modinfo0_def(ModuleName), modinfo1_def(ModuleName) |
+                lists:map(fun(Defun) -> translate_defun(Defun, FEnv) end, Defuns)],
   %% Assemble the module.
   cerl:c_module(CerlModuleName, CerlExports, CerlDefuns).
 
 translate_export({F, A}) ->
   cerl:c_fname(F, A).
 
-translate_defun({'ES:DEFINE', Var, {'ES:LAMBDA', Formals, Body}}) ->
+build_fenv(Defuns) ->
+  lists:foldl(fun build_fenv/2, es_env:empty(), Defuns).
+
+build_fenv({'ES:DEFINE', Var, {'ES:LAMBDA', Formals, _Body}}, FEnv) ->
+  none = es_env:lookup(FEnv, Var), % assert
+  es_env:enter(FEnv, Var, length(Formals)).
+
+translate_defun({'ES:DEFINE', Var, {'ES:LAMBDA', Formals, Body}}, FEnv) ->
   CerlFVar = cerl:c_fname(Var, length(Formals)),
-  CerlFun = translate_lambda(Formals, Body),
+  CerlFun = translate_lambda(Formals, Body, FEnv),
   {CerlFVar, CerlFun}.
 
-translate_expr(AST) ->
+translate_expr(AST, FEnv) ->
   case AST of
     {'ES:GLOVAR', Var} ->
-      translate_glovar(Var);
+      translate_glovar(Var, FEnv);
     {'ES:IF', Pred, Then, Else} ->
-      translate_if(Pred, Then, Else);
+      translate_if(Pred, Then, Else, FEnv);
     {'ES:LAMBDA', Formals, Body} ->
-      translate_lambda(Formals, Body);
+      translate_lambda(Formals, Body, FEnv);
     {'ES:LET', Bindings, Body} ->
-      translate_let(Bindings, Body);
+      translate_let(Bindings, Body, FEnv);
     {'ES:LETREC', Bindings, Body} ->
-      translate_letrec(Bindings, Body);
+      translate_letrec(Bindings, Body, FEnv);
     {'ES:LOCVAR', Var} ->
       translate_locvar(Var);
     {'ES:PRIMOP', PrimOp, Actuals} ->
-      translate_primop(PrimOp, Actuals);
+      translate_primop(PrimOp, Actuals, FEnv);
     {'ES:SEQ', First, Next} ->
-      translate_seq(First, Next);
+      translate_seq(First, Next, FEnv);
     {'ES:QUOTE', Value} ->
       translate_quote(Value)
   end.
 
-translate_glovar(Var) ->
-  %% Synthesize "es_gloenv:get_var(Var)".
-  cerl:c_call(cerl:c_atom('es_gloenv'), cerl:c_atom('get_var'), [cerl:c_atom(Var)]).
+%% Variable references not bound in their top-level defun become ES:GLOVAR.
+%% In a module they can only reference top-level defuns in the same module.
+%% FEnv records those and lets us construct the required F/N fname.
+translate_glovar(Var, FEnv) ->
+  Arity = es_env:get(FEnv, Var),
+  cerl:c_fname(Var, Arity).
 
-translate_if(Pred, Then, Else) ->
+translate_if(Pred, Then, Else, FEnv) ->
   %% Synthesize "case Pred of false -> Else; _ -> Then end".
-  cerl:c_case(translate_expr(Pred),
-              [cerl:c_clause([cerl:c_atom('false')], translate_expr(Else)),
-               cerl:c_clause([wildpat()], translate_expr(Then))]).
+  cerl:c_case(translate_expr(Pred, FEnv),
+              [cerl:c_clause([cerl:c_atom('false')], translate_expr(Else, FEnv)),
+               cerl:c_clause([wildpat()], translate_expr(Then, FEnv))]).
 
-translate_lambda(Formals, Body) ->
-  assemble_lambda(translate_formals(Formals), translate_expr(Body)).
+translate_lambda(Formals, Body, FEnv) ->
+  assemble_lambda(translate_formals(Formals), translate_expr(Body, FEnv)).
 
 assemble_lambda(Vars, Body) ->
   cerl:c_fun(Vars, Body).
@@ -101,14 +114,14 @@ assemble_lambda(Vars, Body) ->
 translate_formals(Formals) ->
   lists:map(fun cerl:c_var/1, Formals).
 
-translate_letrec(Bindings, Body) ->
-  assemble_letrec([translate_letrec_binding(Binding) || Binding <- Bindings],
-                  translate_expr(Body)).
+translate_letrec(Bindings, Body, FEnv) ->
+  assemble_letrec([translate_letrec_binding(Binding, FEnv) || Binding <- Bindings],
+                  translate_expr(Body, FEnv)).
 
-translate_letrec_binding({Var, Formals, Body}) ->
+translate_letrec_binding({Var, Formals, Body}, FEnv) ->
   CerlFormals = translate_formals(Formals),
   Arity = length(CerlFormals),
-  {cerl:c_fname(Var, Arity), CerlFormals, translate_expr(Body)}.
+  {cerl:c_fname(Var, Arity), CerlFormals, translate_expr(Body, FEnv)}.
 
 assemble_letrec(Bindings, Body) ->
   FVars = [FVar || {FVar, _Formals, _Body} <- Bindings],
@@ -122,25 +135,25 @@ assemble_subst([], Body) -> Body;
 assemble_subst([FVar | FVars], Body) ->
   assemble_subst(FVars, cerl:c_let([cerl:c_var(cerl:fname_id(FVar))], FVar, Body)).
 
-translate_let([],  Body) ->
-  translate_expr(Body);
-translate_let([{Lhs, Rhs} | Bindings], Body) ->
-  cerl:c_let([cerl:c_var(Lhs)], translate_expr(Rhs), translate_let(Bindings, Body)).
+translate_let([],  Body, FEnv) ->
+  translate_expr(Body, FEnv);
+translate_let([{Lhs, Rhs} | Bindings], Body, FEnv) ->
+  cerl:c_let([cerl:c_var(Lhs)], translate_expr(Rhs, FEnv), translate_let(Bindings, Body, FEnv)).
 
 translate_locvar(Var) ->
   %% TODO: assumes Var is printable
   cerl:c_var(Var).
 
-translate_primop('ES:APPLY', [Fun | Args]) ->
-  CerlActuals = lists:map(fun translate_expr/1, Args),
+translate_primop('ES:APPLY', [Fun | Args], FEnv) ->
+  CerlActuals = lists:map(fun(Arg) -> translate_expr(Arg, FEnv) end, Args),
   case Fun of
     {'ES:PRIMOP', 'ES:COLON', [M, F, {'ES:QUOTE', A}]} when A =:= length(Args) ->
-      cerl:c_call(translate_expr(M), translate_expr(F), CerlActuals);
+      cerl:c_call(translate_expr(M, FEnv), translate_expr(F, FEnv), CerlActuals);
     _ ->
-      cerl:c_apply(translate_expr(Fun), CerlActuals)
+      cerl:c_apply(translate_expr(Fun, FEnv), CerlActuals)
   end;
-translate_primop(PrimOp, Args0) ->
-  CerlArgs = lists:map(fun translate_expr/1, Args0),
+translate_primop(PrimOp, Args0, FEnv) ->
+  CerlArgs = lists:map(fun(Arg) -> translate_expr(Arg, FEnv) end, Args0),
   case {PrimOp, CerlArgs} of
     {'ES:COLON', [M, F, A]} -> make_fun(M, F, A);
     {'ES:LIST', _} -> make_list(CerlArgs)
@@ -152,8 +165,8 @@ make_fun(M, F, A) ->
 make_list([]) -> cerl:c_nil();
 make_list([H | T]) -> cerl:c_cons(H, make_list(T)).
 
-translate_seq(First, Next) ->
-  cerl:c_seq(translate_expr(First), translate_expr(Next)).
+translate_seq(First, Next, FEnv) ->
+  cerl:c_seq(translate_expr(First, FEnv), translate_expr(Next, FEnv)).
 
 translate_quote(Value) ->
   %% This is a PRE for cerl:abstract/1, but may not be true for all possible
